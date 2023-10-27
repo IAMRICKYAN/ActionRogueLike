b5c706cfc69ae9a9d06d6fdf1e632c791fcb3bc1
R1ckYan <rickyan.s@foxmail.com>
2023-10-27 20:04:19 +0800

    SMagicProtile

---
+++ Source/ActionRogueLike/Public/SMagicProjectile.h
@@ -0,0 +1,47 @@
+ 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UParticleSystemComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASMagicProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASMagicProjectile();
+
+	
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* SphereComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UProjectileMovementComponent* MovementComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* EffectComp;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
