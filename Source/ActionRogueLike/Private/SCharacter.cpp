dda51a8dc08a877daf41da25618211eb5337a04c
R1ckYan <rickyan.s@foxmail.com>
2023-11-07 17:29:51 +0800

    完成Projectile的爆炸,以及Debug使用的投射物蓝图

--- Source/ActionRogueLike/Private/SCharacter.cpp
+++ Source/ActionRogueLike/Private/SCharacter.cpp
@@ -91,6 +91,7 @@ void ASCharacter::PrimaryAttack_TimeElasped()
     
     	FActorSpawnParameters SpawnParams;
     	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		SpawnParams.Instigator = this;
     	
     	GetWorld()->SpawnActor<AActor>(ProjectileClass,SpawnTM,SpawnParams);
 }
