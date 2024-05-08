cac491031955c2e681566b7148eb11703648b427
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-08 19:41:07 +0800

    使 Bot 根据范围进行攻击

---
+++ Source/ActionRogueLike/Public/AI/SBTTask_RangedTask.h
@@ -0,0 +1,27 @@
+//SBTTask_RangedTask.h
+// Fill out your copyright notice in the Description page of Project Settings.
+ 
+#pragma once
+ 
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SBTTask_RangedTask.generated.h"
+ 
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USBTTask_RangedTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+ 
+ 
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+ 
+ 
+protected:
+ 
+	UPROPERTY(EditAnywhere,Category="AI")
+	TSubclassOf<AActor> ProjectileClass;
+};
+ 
