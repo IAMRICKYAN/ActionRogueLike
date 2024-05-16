56d2166e55c5bb58895c872f8250b7226d237eef
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-05-16 21:58:39 +0800

    注释

--- Source/ActionRogueLike/Public/SMagicProjectile.h
+++ Source/ActionRogueLike/Public/SMagicProjectile.h
@@ -28,13 +28,16 @@ protected:
 	UFUNCTION()
 	void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	
+
+	// 球体,用于计算碰撞
 	UPROPERTY(BlueprintReadOnly,VisibleAnywhere)
 	USphereComponent* SphereComp;
 
+	// 投射体，控制球体的运动
 	UPROPERTY(BlueprintReadOnly,VisibleAnywhere)
 	UProjectileMovementComponent* MovementComp;
 
+	// 粒子系统，控制特效
 	UPROPERTY(BlueprintReadOnly,VisibleAnywhere)
 	UParticleSystemComponent* EffectComp;
 
