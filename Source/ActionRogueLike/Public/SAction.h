bb2a595bd02174127d72b4b5d375727af756e69a
YAN ZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2024-09-18 15:38:41 +0800

    设置游戏中的Tags(Attacking,Sprinting)

--- Source/ActionRogueLike/Public/SAction.h
+++ Source/ActionRogueLike/Public/SAction.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "UObject/NoExportTypes.h"
 #include "SAction.generated.h"
 
@@ -13,6 +14,18 @@ UCLASS(Blueprintable)
 class ACTIONROGUELIKE_API USAction : public UObject
 {
 	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Action")
+	USActionComponent* GetOwningComponent() const;
+	
+	/* Tags added to owning actor when activated, removed when action stops */
+	UPROPERTY(EditDefaultsOnly, Category = "Tags")
+	FGameplayTagContainer GrantsTags;
+
+	/* Action can only start if OwningActor has none of these Tags applied */
+	UPROPERTY(EditDefaultsOnly, Category = "Tags")
+	FGameplayTagContainer BlockedTags;
 
 
 public:
